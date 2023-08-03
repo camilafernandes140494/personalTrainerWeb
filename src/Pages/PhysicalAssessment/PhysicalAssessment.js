886cb4b7e0215b1b832bbc8234567ea505369c8a
Camila <camila.silva@nappsolutions.com>
2023-08-02 22:08:52 -0300

    teste

--- src/Pages/PhysicalAssessment/PhysicalAssessment.js
+++ src/Pages/PhysicalAssessment/PhysicalAssessment.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../../Service/Connection/AuthContext"
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Service/Connection/AuthContext';
 
 function PhysicalAssessment() {
   const { user, loading, logout } = useContext(AuthContext);
@@ -13,13 +13,16 @@ function PhysicalAssessment() {
   }
 
   return (
-      <div
-        style={{
-          borderRadius: "10%",
-          backgroundColor: "rgba(217, 217, 217, 0.4)",
-        }}>
-          
-      <p style={{color:'black'}}>Bem-vindo, {user.email}!</p> {/* Exibe uma mensagem de boas-vindas com o email do usuário autenticado */}
+    <div
+      style={{
+        borderRadius: '10%',
+        backgroundColor: 'rgba(217, 217, 217, 0.4)',
+      }}
+    >
+      <p style={{ color: 'black' }}>Bem-vindo, {user.email}!</p>{' '}
+      {/* Exibe uma mensagem de boas-vindas com o email do usuário autenticado */}
+      <p style={{ color: 'black' }}>Bem-vindo, {user.email}!</p>{' '}
+      {/* Exibe uma mensagem de boas-vindas com o email do usuário autenticado */}
       <button onClick={logout}>Logout</button> {/* Exibe um botão de logout */}
     </div>
   );
